0f610a7307be160fa42a3e5ca890e854bdf07e96
Shash <ashishsinghlogic@gmail.com>
2025-06-28 18:29:57 +0530

    Add canvas initialization and draw function to home screen
    Introduced useEffect to initialize the canvas context, set line width, and adjust canvas size on mount. Added a draw function to handle drawing on the canvas with white strokes.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,25 @@
 //making a HTMLcanvas with ref hook
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 
 
 export default function index() {
 const canvasRef = useRef<HTMLCanvasElement>(null)
 const [isDrawing, setIsDrawing] = useState(false)
 
+
+//mounting the canvas with useEffect
+useEffect(()=>{
+const canvas = canvasRef.current;
+if(canvas){
+    const context = canvas.getContext("2d")
+        if(context){
+        context.lineWidth = 5; //setting the line width
+        context.lineCap = "round" //setting the line cap
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight - canvas.offsetTop; //setting the height of the canvas
+        }
+    }
+}, []);
 
 const startDrawing = (e : React.MouseEvent<HTMLCanvasElement>) =>{
     const canvas = canvasRef.current;
@@ -26,6 +40,17 @@ const stopDrawing = () =>{
     setIsDrawing(false)
 }
 
+const draw =(e: HTMLCanvasElement)=>{
+    const canvas = canvasRef.current;
+    if(canvas){
+        const context = canvas.getContext("2d")
+        if(context){
+            context.strokeStyle = "white"
+            context.lineTo(e.nativeEvent.clientX, e.nativeEvent.clientY)
+            context.stroke()
+        }
+    }
+}
 
 
 
