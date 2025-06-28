62b052e5337d7743c022bdea14fbd6ed6e4bc3fe
Shash <ashishsinghlogic@gmail.com>
2025-06-28 19:12:34 +0530

    Add result state and sendData function to home screen
    Introduced new state variables for result and dictOfVars, defined Response and result interfaces, and implemented the sendData async function to post canvas data and variables to the API. Also improved resetCanvas to clear the canvas context safely.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -7,23 +7,57 @@ import axios from "axios"
 
 
 
+interface Response {
+    expr: string;
+    result: string;
+    assing: boolean;
+};
+
+interface result{
+    expression : string;
+    result: string;
+}
+
 
 const [color, setColor] = useState('rgb(255, 255, 255)');
 const[reset, setReset] = useState(false);
+const[result, setResult] = useState<result>({expression: "", result: ""});
+const[dictOfVars, setDictOfVars] = useState({});
+
+
+const sendData = async ()=>{
+    const canvas = canvasRef.current;
+    if(canvas){
+        const response = await axios({
+            method : "post",
+            url : `${import.meta.env.VITE_API_URL}/calculate`,
+            data:{
+                data: canvas.toDataURL('image/png'),
+                dict_of_vars : dictOfVars,
+            }
+        })
+        const data = response.data as Response;
+        console.log('data', data);
+}
 
 useEffect(()=>{
 if(reset){
     resetCanvas();
     setReset(false);
 }
-}[reset]);
+},[reset]);
 //using reset as dependency array because the canvas needs to be 
 //reset when the reset button is clicked
+
+
 
 const resetCanvas = () =>{
     const canvas = canvasRef.current;
     if(canvas){
         const context = canvas.getContext("2d")
+        if(context){
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
     }
 }
 
